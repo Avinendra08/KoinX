417a7ea260881d330a0ef8ff9a80bcbe7eab1f9e
Avinendra <avinendra08@gmail.com>
2024-10-10 10:34:15 +0530

    removed routes for cryptoDataImportController, as it is background task

--- src/routes/crypto.routes.js
+++ src/routes/crypto.routes.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { cryptoDataImportController } from "../controllers/crypto.controller.js";
+
 
 const router = express.Router();
 
-router.get("/importData",cryptoDataImportController);
+
 
 export default router;
